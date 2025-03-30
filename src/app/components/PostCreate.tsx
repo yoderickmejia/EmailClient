df65b7720d082e15d726025ab0ae338737a1b5e2
Yoderick Montero <yoderickmejia@gmail.com>
2025-03-29 20:02:15 -0400

    configuration

---
+++ src/app/components/PostCreate.tsx
@@ -0,0 +1,38 @@
+"use client"; 
+import React, { useState } from "react";
+import axios from "axios";
+
+const PostCreate = () => {
+  const [title, setTitle] = useState("");
+
+  const onSubmit = async (event:React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      await axios.post(`http://localhost:4000/posts`, { title });
+      setTitle("");
+    } catch (error) {
+      console.error("Error al enviar el post:", error);
+    }
+
+    setTitle("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default PostCreate;
