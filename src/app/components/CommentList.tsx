310c380763d400dda3ea4c08bf59f0ca0a28582d
Yoderick Montero <ymontero@gruporead.com>
2025-04-14 12:12:25 -0400

    arreglando los pendings

--- src/app/components/CommentList.tsx
+++ src/app/components/CommentList.tsx
@@ -1,19 +1,12 @@
 import React from "react";
 
 const CommentList: React.FC<{ comments: Array<string> }> = ({ comments }) => {
- 
-  console.log("comments", comments)
+  console.log("comments", comments);
   const renderedComments = comments.map((comment: any) => {
-    return <li key={comment.id}>{comment.content}</li>
-    })
+    return <li key={comment.id}>{comment.content}</li>;
+  });
 
-
-    return (
-        <ul>
-            {renderedComments}
-        </ul>
-    )
+  return <ul>{renderedComments}</ul>;
 };
-
 
 export default CommentList;
