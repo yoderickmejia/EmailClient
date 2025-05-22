e62efd4e8dfd527cc99d559b2e631577c91dd215
Yoderick Montero <ymontero@gruporead.com>
2025-05-22 12:03:23 -0400

    dockerizando

--- src/app/components/CommentList.tsx
+++ src/app/components/CommentList.tsx
@@ -3,6 +3,19 @@ import React from "react";
 const CommentList: React.FC<{ comments: Array<string> }> = ({ comments }) => {
   console.log("comments", comments);
   const renderedComments = comments.map((comment: any) => {
+    let content 
+
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+
+    }
+    if (comment.status === "rejected") {
+        content = "This comment has been rejected";
+    }
+    
     return <li key={comment.id}>{comment.content}</li>;
   });
 
